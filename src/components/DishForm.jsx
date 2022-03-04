7ba05b9d36543ce5f3ef067c571088affe127d62
SorrowOfMind <sortoflifecraze@gmail.com>
2022-03-04 21:00:13 +0100

    structure

---
+++ src/components/DishForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DishForm = () => {
+  return (
+    <div>DishForm</div>
+  )
+}
+
+export default DishForm
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
+import {DishForm, Notification} from './components';
+
+const Routing = () => {
+    return (
+        <Routes>
+        </Routes>
+    );
+}
+
+export default Routing;
